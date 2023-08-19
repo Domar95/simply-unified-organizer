7bff8d675e67f8ec165a9e8c2ae6c56d9bb619b1
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2023-08-19 20:23:57 +0200

    Update backend project structure

---
+++ backend/models/record.py
@@ -0,0 +1,8 @@
+class Record():
+
+    def __init__(self, id, name, importance, description):
+        self.id = id, #UUID
+        self.name = name
+        self.importance = importance
+        self.description = description
+
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,4 @@
-import json
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,51 +8,3 @@ CORS(app)
 
 
 # run with 'flask run' from backend folder
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-class Record():
-
-    def __init__(self, id, name, importance, description):
-        self.id = id, #UUID
-        self.name = name
-        self.importance = importance
-        self.description = description
-
-
-def get_all_records():
-    records = [
-        Record('1', 'record 1', 5, 'some description'),
-        Record('2', 'record 2', 8, 'some description'),
-        Record('3', 'record 3', 2, 'some description'),
-             ]
-    return records
-
-def get_record():
-    record = Record('1', 'record 1', 5, 'some description')
-    return record
-
-@app.route("/records", methods=['GET'])
-def records_api():
-    # record = get_record()
-    # response = jsonify(name = record.name,
-    #     importance = record.importance,
-    #     description = record.description)
-    # response = json.dumps({"result": record}), 200
-    records = get_all_records()
-    serialized_records = [record.__dict__ for record in records]
-    return jsonify(serialized_records)
-    # response = [jsonify(name = record.name,
-    #         importance = record.importance,
-    #         description = record.description) for record in records]
-    # response = jsonify(results=record, status_code=200)
-    #response.status_code = 200 # or 400 or whatever
-    #return response
-    # return [
-    #     jsonify(
-    #     name = record.name,
-    #     importance = record.importance,
-    #     description = record.description)
-    #     for record in records]
