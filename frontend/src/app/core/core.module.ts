d4f257caf0e420267b41561a74cf672d8f2b76f7
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-05-31 22:22:37 +0200

    ORGA-4 Add & use Icon Registry Service
    it is used to add custom icons

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { IconRegistryService } from './services/icon-registry.service';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    NavbarComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [NavbarComponent, FooterComponent]
+  declarations: [FooterComponent, NavbarComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [NavbarComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(private iconRegistry: IconRegistryService) {
+    this.iconRegistry.registerIcons([
+      { name: 'notebook_icon', url: './assets/icons/notebook_icon.svg' },
+    ]);
+  }
+}
---
+++ frontend/src/app/core/services/icon-registry.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IconRegistryService {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
+
+  public registerIcons(icons: { name: string; url: string }[]): void {
+    icons.forEach((icon) => {
+      this.matIconRegistry.addSvgIcon(
+        icon.name,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(icon.url)
+      );
+    });
+  }
+}
