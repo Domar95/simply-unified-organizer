776f46f2a69d5dfc4ed90a91cfc67d3609b86212
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-09-01 20:49:58 +0200

    feat: scaffold knowledge component

--- frontend/src/app/feature/records/components/index.ts
+++ frontend/src/app/feature/records/components/index.ts
@@ -2,3 +2,4 @@ export * from './records.component';
 export * from './ui-elements/records-form/records-form.component';
 export * from './ui-elements/records-table/records-table.component';
 export * from './record-types/programming-project/programming-project.component';
+export * from './record-types/knowledge/knowledge.component';
