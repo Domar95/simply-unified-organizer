813160f18278ee8ebf4105c3869a4dacc85a1a59
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-08 22:28:18 +0100

    build: remove flask_restful

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -4,7 +4,6 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
@@ -16,8 +15,7 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("local_db_uri")
 
-    api = Api(app)
-    add_resources(api, app)
+    add_resources(app)
 
     # Initialize logging
     logging.basicConfig(level=logging.INFO)
@@ -34,12 +32,10 @@ def create_app() -> Flask:
     # to solve the error related to the Cross-Origin Resource Sharing (CORS) policy implemented by web browsers to ensure security when making requests from one domain (origin) to another
     CORS(app)
 
-    # create_api(app)
-
     return app
 
 
-def add_resources(api: Api, app: Flask) -> None:
+def add_resources(app: Flask) -> None:
     from src.resources.records.programming_project import (
         programming_project_view,
         programming_project_list_view,
@@ -50,13 +46,11 @@ def add_resources(api: Api, app: Flask) -> None:
         view_func=programming_project_view,
         methods=["GET", "PATCH", "DELETE"],
     )
-
     app.add_url_rule(
         "/records/programming-project",
         view_func=programming_project_view,
         methods=["POST"],
     )
-
     app.add_url_rule(
         "/records/programming-project",
         view_func=programming_project_list_view,
@@ -70,13 +64,11 @@ def add_resources(api: Api, app: Flask) -> None:
         view_func=knowledge_view,
         methods=["GET", "PUT", "DELETE"],
     )
-
     app.add_url_rule(
         "/records/knowledge",
         view_func=knowledge_view,
         methods=["POST"],
     )
-
     app.add_url_rule(
         "/records/knowledge",
         view_func=knowledge_list_view,
