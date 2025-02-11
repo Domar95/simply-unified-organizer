d387fbaf908bcf3d5538d2310197549de8a33d9e
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-11 23:52:08 +0100

    feat(record-strategy): add updateRecord()

--- frontend/src/app/feature/records/services/strategies/record-strategy.interface.ts
+++ frontend/src/app/feature/records/services/strategies/record-strategy.interface.ts
@@ -1,10 +1,39 @@
-import { KnowledgeApiResponse, KnowledgePostRequest, NoteApiResponse, NotePostRequest, ProgrammingProjectApiResponse, ProgrammingProjectPostRequest, QuestionBase } from "@feature/records/models";
+import {
+  KnowledgeApiResponse,
+  KnowledgePatchRequest,
+  KnowledgePostRequest,
+  NoteApiResponse,
+  NotePatchRequest,
+  NotePostRequest,
+  ProgrammingProjectApiResponse,
+  ProgrammingProjectPatchRequest,
+  ProgrammingProjectPostRequest,
+  QuestionBase,
+} from '@feature/records/models';
 
 export interface RecordStrategy {
   /* API */
-  getRecord(id: string): Promise<KnowledgeApiResponse | NoteApiResponse | ProgrammingProjectApiResponse>;
-  addRecord(data: KnowledgePostRequest | NotePostRequest | ProgrammingProjectPostRequest): Promise<KnowledgeApiResponse | NoteApiResponse | ProgrammingProjectApiResponse>;
+  getRecord(
+    id: string
+  ): Promise<
+    KnowledgeApiResponse | NoteApiResponse | ProgrammingProjectApiResponse
+  >;
+  addRecord(
+    data: KnowledgePostRequest | NotePostRequest | ProgrammingProjectPostRequest
+  ): Promise<
+    KnowledgeApiResponse | NoteApiResponse | ProgrammingProjectApiResponse
+  >;
+  updateRecord(
+    id: string,
+    data:
+      | KnowledgePatchRequest
+      | NotePatchRequest
+      | ProgrammingProjectPatchRequest
+  ): Promise<
+    KnowledgeApiResponse | NoteApiResponse | ProgrammingProjectApiResponse
+  >;
 
-  getQuestions(initialValues?: Record<string, string | number>): QuestionBase<string | number | Date>[];
+  getQuestions(
+    initialValues?: Record<string, string | number>
+  ): QuestionBase<string | number | Date>[];
 }
-
