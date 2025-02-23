e4b40d167a4213e0d16a046decfec5d5b9527dd3
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-23 16:43:28 +0100

    feat(login): add initial login form

---
+++ frontend/src/app/feature/auth/components/login/login-form/login-form.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'suo-login-form',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss',
+})
+export class LoginFormComponent {
+  loginForm = new FormGroup({
+    username: new FormControl<string>('', Validators.required),
+    password: new FormControl<string>('', Validators.required),
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  get requiredErrorMessage() {
+    return 'This field is required';
+  }
+}
