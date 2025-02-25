d1c45c5967ac1bec53de5e9fb8a8c9b4636887fd
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2025-02-25 19:55:57 +0100

    refactor(login-api): remove jsonify()
    its applied automatically

--- backend/src/resources/auth/login.py
+++ backend/src/resources/auth/login.py
@@ -12,7 +12,7 @@ class UserLogin(MethodView):
     def post(self):
         data = request.get_json()
         if not data or "username" not in data or "password" not in data:
-            return jsonify(error="Missing username or password"), 400
+            return {"error": "Missing username or password"}, 400
 
         username = data.get("username")
         password = data.get("password")
@@ -21,9 +21,9 @@ class UserLogin(MethodView):
         if user and user.check_password(password):
             validated_user = UserSchema.model_validate(user)
             access_token = create_access_token(identity=user.id)
-            return jsonify(access_token=access_token, user=validated_user.model_dump())
+            return {"access_token": access_token, "user": validated_user.model_dump()}
 
-        return jsonify(error="Invalid credentials"), 401
+        return {"error": "Invalid credentials"}, 401
 
 
 user_login_view = UserLogin.as_view("user_login_view")
